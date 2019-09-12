b97acbb9eb62836339828232eb6b60d6a2b57437
WORMSS <wormssmail@gmail.com>
2019-09-12 08:24:04 +0100

    FilePath to variable

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { appendFileSync, existsSync, readFileSync, writeFileSync } from 'fs';
+import { EOL } from 'os';
 import { SequenceValue } from './types';
 import { sendMailWord } from './email';
 
@@ -7,26 +8,31 @@ const WORDS = (prefixed: number) => `./words.${prefixed}.txt`;
 let knownWords: Set<string> | null = null;
 
 export function getLastPoint(prefixed: number): SequenceValue {
-  if (existsSync(LAST_POINT(prefixed))) {
-    const fileStr = readFileSync(LAST_POINT(prefixed), 'utf-8');
+  const lastPointFilePath = LAST_POINT(prefixed);
+  if (existsSync(lastPointFilePath)) {
+    const fileStr = readFileSync(lastPointFilePath, 'utf-8');
     if (fileStr === '') {
-      throw new Error(`${LAST_POINT} is empty`);
+      throw new Error(`${lastPointFilePath} is empty`);
     }
     const lastPoint = JSON.parse(fileStr);
     if (!Array.isArray(lastPoint)) {
-      throw new Error(`${LAST_POINT} is invalid, not array`);
+      throw new Error(`${lastPointFilePath} is invalid, not array`);
     }
     if (lastPoint.length !== 15) {
-      throw new Error(`${LAST_POINT} is invalid, array not 16 in length`);
+      throw new Error(
+        `${lastPointFilePath} is invalid, array not 16 in length`,
+      );
     }
     if (!isArrayOfNumbers(lastPoint)) {
-      throw new Error(`${LAST_POINT} is invalid, values are not numbers`);
+      throw new Error(
+        `${lastPointFilePath} is invalid, values are not numbers`,
+      );
     }
     if (!lastPoint.every((v) => v >= 0 && v < 16)) {
-      throw new Error(`${LAST_POINT} is invalid, number out of range`);
+      throw new Error(`${lastPointFilePath} is invalid, number out of range`);
     }
     if (!lastPoint.every((v, index, array) => array.indexOf(v) === index)) {
-      throw new Error(`${LAST_POINT} is invalid, not unique`);
+      throw new Error(`${lastPointFilePath} is invalid, not unique`);
     }
     return lastPoint as SequenceValue;
   }
@@ -36,9 +42,11 @@ export function getLastPoint(prefixed: number): SequenceValue {
   }
 }
 export function storeLastPoint(prefixed: number, value: SequenceValue): void {
-  writeFileSync(LAST_POINT(prefixed), JSON.stringify(value), 'utf-8');
+  const lastPointFilePath = LAST_POINT(prefixed);
+  writeFileSync(lastPointFilePath, JSON.stringify(value), 'utf-8');
 }
 export function storeWords(prefixed: number, arrayOfValues: string[][]): void {
+  const wordsFilePath = WORDS(prefixed);
   if (arrayOfValues.length === 0) {
     return;
   }
@@ -53,13 +61,14 @@ export function storeWords(prefixed: number, arrayOfValues: string[][]): void {
     if (value.length > 13 && !(knownWords && knownWords.has(value))) {
       if (knownWords === null) {
         knownWords = new Set(
-          existsSync(WORDS(prefixed))
-            ? readFileSync(WORDS(prefixed), 'utf-8').split(/\r?\n/)
+          existsSync(wordsFilePath)
+            ? readFileSync(wordsFilePath, 'utf-8').split(/\r?\n/)
             : undefined,
         );
       }
+      // tslint:disable-next-line: no-console
       console.log(value);
-      appendFileSync(WORDS(prefixed), value + '\n', 'utf-8');
+      appendFileSync(wordsFilePath, value + '\n', 'utf-8');
       if (value.length > 14) {
         sendMailWord(value);
       }
